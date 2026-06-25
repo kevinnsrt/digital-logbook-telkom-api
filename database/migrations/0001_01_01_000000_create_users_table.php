04b8109da1bda8cc7c9dfd22c0ca6f30846460f3
kevinnsrt <siraitkevin25@gmail.com>
2026-06-25 16:46:15 +0700

    adding endpoint register from flutter

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', ['admin', 'user','super_admin'])->default('user');
+            $table->enum('role', ['admin', 'user','super_admin']);
             $table->rememberToken();
             $table->timestamps();
         });
