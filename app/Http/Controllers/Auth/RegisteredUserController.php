32c3c2a36721e052f13b997970e71eaa5e25c5f9
kevinnsrt <siraitkevin25@gmail.com>
2026-06-25 16:54:00 +0700

    adding endpoint register from flutter

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -55,7 +55,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'role'=> 'required,in:admin,superadmin,user',
+            'role'=> 'required',
         ]);
 
         $user = User::create([
